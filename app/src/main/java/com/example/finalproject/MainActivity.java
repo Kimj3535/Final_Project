c4c42800974a9873e5fbfb25e05527e4bd288f35
wiam95 <wiam95@hotmail.com>
2020-12-09 15:30:49 -0500

    fixing my branch

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button goToCovidActivity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToCovidActivity = findViewById(R.id.goToCovidActivity);
+
+
+
+        //This creates a transition to load CovidQuery
+        final Intent covidActivity = new Intent(this, MainActivity.class);
+
+        //When you click the search button, start the next activity
+        covidActivity.setOnClickListener( click ->
+        {
+            startActivity(covidActivity);
+        });
+
     }
 }
